83161f57f5791feede302f94c197c9011f119275
G00244090 <g00244090@gmit.ie>
2016-04-09 17:06:34 +0100

    addition of spinners
    added spinners for selection

--- src/com/example/Service_App/UpdateActivity.java
+++ src/com/example/Service_App/UpdateActivity.java
@@ -2,8 +2,12 @@ package com.example.Service_App;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UpdateActivity extends Activity {
 
@@ -29,6 +33,28 @@ public class UpdateActivity extends Activity {
         road = (Spinner) findViewById(R.id.road);
         list = (Spinner) findViewById(R.id.list);
         speed = (Spinner) findViewById(R.id.speed);
+        List<String> CountyArray =  new ArrayList<String>();
+        List<String> RoadArray =  new ArrayList<String>();
+        List<String> TypeArray =  new ArrayList<String>();
+        List<String> SpeedArray =  new ArrayList<String>();
+        ArrayAdapter<String> countyAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, CountyArray);
+        ArrayAdapter<String> RoadAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, RoadArray);
+        ArrayAdapter<String> TypeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, TypeArray);
+        ArrayAdapter<String> SpeedAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, SpeedArray);
+
+        CountyArray.add("Dublin");
+        CountyArray.add("Galway");
+        CountyArray.add("Mayo");
+
+        RoadArray.add("National Route");
+        RoadArray.add("Motorway Regional");
+        RoadArray.add("Regional Route");
+
+        countyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        RoadAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        county.setAdapter(countyAdapter);
+        road.setAdapter(RoadAdapter);
 
     }// onCreate
 
